7d83004353d008a78cd90c886e8c482e4003ee8d
lisp719 <shou.suzuki0@gmail.com>
2015-11-14 01:37:10 +0900

    Update etc_tools.bat

--- etc_tools.bat
+++ etc_tools.bat
@@ -1,6 +1,6 @@
 curl http://download.forest.impress.co.jp/pub/library/c/clibor/10713/clibor.zip -o clibor.zip
 curl http://download.sysinternals.com/files/Ctrl2Cap.zip -o Ctrl2Cap.zip
-curl https://files.gyazo.com/setup/Gyazo-3.1.6.exe gyazo.exe
+curl https://files.gyazo.com/setup/Gyazo-3.1.6.exe -o gyazo.exe
 
 powershell expand-archive clibor.zip
 powershell expand-archive Ctrl2Cap.zip
