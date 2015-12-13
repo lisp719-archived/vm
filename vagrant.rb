43040352fae41dde20a54f6d98268d6bcda2b980
lisp719 <shou.suzuki0@gmail.com>
2015-12-13 09:56:31 +0900

    Update vagrant.rb

--- vagrant.rb
+++ vagrant.rb
@@ -18,11 +18,8 @@ Vagrant.configure(2) do |config|
   config_script = '
     echo >> ~/.bash_profile
     curl https://raw.githubusercontent.com/lisp719/conf/master/linux_files/add_bash_profile >> ~/.bash_profile
-
     curl https://raw.githubusercontent.com/lisp719/conf/master/linux_files/.tmux.conf -o ~/.tmux.conf
-
     curl https://raw.githubusercontent.com/lisp719/conf/master/linux_files/.gemrc -o ~/.gemrc
-
     curl https://raw.githubusercontent.com/lisp719/conf/master/linux_files/.npmrc -o ~/.npmrc
   '
 
