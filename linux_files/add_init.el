792be78db9a9cb45b0f190f01fa0576e31c7dfb0
lisp719 <lisp719@gmail.com>
2015-12-19 17:18:02 +0900

    .spacemacs

--- linux_files/add_init.el
+++
@@ -1,9 +0,0 @@
-(unless (package-installed-p 'inf-clojure)
-  (package-refresh-contents)
-  (package-install 'inf-clojure))
-
-(defun figwheel-repl ()
-  (interactive)
-  (run-clojure "lein figwheel"))
-
-(add-hook 'clojure-mode-hook #'inf-clojure-minor-mode)
