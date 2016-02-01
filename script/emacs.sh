3ca1a40a7b526f2f1e66aad9e1e5b80f600f2d58
lisp719 <lisp719@gmail.com>
2016-02-02 01:27:50 +0900

    break

--- script/emacs.sh
+++ script/emacs.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
 sudo pacman -S --noconfirm emacs
+
 git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d
 curl https://raw.githubusercontent.com/lisp719/conf/master/linux_files/.spacemacs -o ~/.spacemacs
 emacs --daemon
