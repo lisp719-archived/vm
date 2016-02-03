489e88086a129c7000ea3ac9b38252d53fc47294
Syo Suzuki <shou.suzuki0@gmail.com>
2016-02-03 16:32:16 +0900

    not install ruby

--- script/rbenv.sh
+++ script/rbenv.sh
@@ -6,16 +6,5 @@ echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
 
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
-source ~/.bash_profile
-rbenv install 2.2.4
-rbenv global 2.2.4
-
 echo install: -N >> .gemrc
 echo update: -N >> .gemrc
-
-gem i \
-  bundler \
-  thor
-
-git clone https://github.com/lisp719/cli.git ~/bin/cli
-echo 'PATH=$HOME/bin/cli:$PATH' >> ~/.bash_profile
