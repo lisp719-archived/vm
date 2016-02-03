b816f1f8f5436c72de1702dc9f14193199098139
Syo Suzuki <shou.suzuki0@gmail.com>
2016-02-03 16:30:46 +0900

    rename

--- Vagrantfile.rb
+++ Vagrantfile.rb
@@ -9,7 +9,7 @@ Vagrant.configure(2) do |config|
   end
 
   config.vm.provision "shell", privileged: false, path: "script/base.sh"
-  config.vm.provision "shell", privileged: false, path: "script/ruby.sh"
+  config.vm.provision "shell", privileged: false, path: "script/rbenv.sh"
   config.vm.provision "shell", privileged: false, path: "script/db.sh"
   config.vm.provision "shell", privileged: false, path: "script/lein.sh"
   config.vm.provision "shell", privileged: false, path: "script/go.sh"
