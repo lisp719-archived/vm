c22424cf1766b1cf66bc303691ee6ce809c26b5b
lisp719 <shou.suzuki0@gmail.com>
2016-02-07 01:33:22 +0900

    Update go.sh

--- script/go.sh
+++ script/go.sh
@@ -5,3 +5,8 @@ sudo pacman -S --noconfirm go
 mkdir ~/go
 echo 'export GOPATH=~/go' >> ~/.bash_profile
 echo 'export PATH=$PATH:~/go/bin' >> ~/.bash_profile
+
+go get -u -v github.com/nsf/gocode
+go get -u -v github.com/rogpeppe/godef
+go get -u -v golang.org/x/tools/cmd/oracle
+go get -u -v golang.org/x/tools/cmd/gorename
