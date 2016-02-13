28726b5bbe775d19f73a7cd1c27849a1d344f341
lisp719 <shou.suzuki0@gmail.com>
2016-02-13 13:06:21 +0900

    Update go.sh

--- script/go.sh
+++ script/go.sh
@@ -12,3 +12,5 @@ go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/oracle
 go get -u -v golang.org/x/tools/cmd/gorename
+
+go get github.com/peco/peco/cmd/peco
