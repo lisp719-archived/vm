d5c92e935a9aad21faa66a6d55faba606d3fc9b5
lisp719 <shou.suzuki0@gmail.com>
2016-04-06 00:32:38 +0900

    Update rbenv.sh

--- script/rbenv.sh
+++ script/rbenv.sh
@@ -5,3 +5,6 @@ git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 
 echo install: -N >> .gemrc
 echo update: -N >> .gemrc
+
+# curl -fsSL https://gist.github.com/mislav/055441129184a1512bb5.txt | rbenv install --patch 2.1.5
+# rbenv global 2.1.5
