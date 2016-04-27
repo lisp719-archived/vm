7b4289061ea33d68aa18a48969a3c6c764ef3c68
suzuki syo <lisp719@hotmail.co.jp>
2016-04-28 00:58:48 +0900

    mysql

--- script/db.sh
+++ script/db.sh
@@ -10,4 +10,7 @@ sudo apt-get install -y \
   mysql-server-5.6 \
   postgresql
 
+sudo sed -i.bak -e "/^\[mysqld\]$/a table_definition_cache = 400" /etc/mysql/my.cnf
+sudo service mysql restart
+
 sudo su -c "createuser -s vagrant" postgres
