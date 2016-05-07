20295c06e8178d27a30951130844025a1f7e2bc9
suzuki syo <lisp719@hotmail.co.jp>
2016-05-07 14:04:52 +0900

    subl setting

--- script/gui.sh
+++ script/gui.sh
@@ -13,3 +13,9 @@ cp ~/linux_files/fcitx.profile ~/.config/fcitx/profile
 
 wget https://download.sublimetext.com/sublime-text_build-3103_amd64.deb
 sudo dpkg -i sublime-text_build-3103_amd64.deb
+rm sublime-text_build-3103_amd64.deb
+
+wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages/
+
+mkdir -p ~/.config/sublime-text-3/Packages/User/
+cp ~/linux_files/subl/* ~/.config/sublime-text-3/Packages/User/
