d14ce3298bb4071141ad3706b524cbe6db2d0ff0
lisp719 <test@example.com>
2016-05-09 17:21:12 +0900

    refactor mysql setting

--- script/db.sh
+++ script/db.sh
@@ -1,9 +1,6 @@
 #! /bin/sh
 
-echo "mysql-server-5.6 mysql-server/root_password password root" | sudo debconf-set-selections
-echo "mysql-server-5.6 mysql-server/root_password_again password root" | sudo debconf-set-selections
-
-sudo apt-get install -y \
+sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
   libpq-dev \
   libmysqlclient-dev \
   libsqlite3-dev \
@@ -12,7 +9,6 @@ sudo apt-get install -y \
 
 sudo sed -i.bak -e "/^\[mysqld\]$/a table_definition_cache = 400" /etc/mysql/my.cnf
 sudo sed -i.bak -e "/^\[mysqld\]$/a default-time-zone = '+0:00'" /etc/mysql/my.cnf
-mysql -uroot -proot mysql -e "SET PASSWORD = PASSWORD('')"
 sudo service mysql restart
 
 sudo su -c "createuser -s vagrant" postgres
