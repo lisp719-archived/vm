9b4ee4a1e287d2bf9877277240524efd3f8b3dd0
lisp719 <lisp719@hotmail.co.jp>
2016-12-01 01:25:22 +0900

    sort

--- run.bat
+++ run.bat
@@ -1,8 +1,8 @@
 REM vagrant destroy -f
 REM vagrant up
+REM vagrant reload
 REM vagrant snapshot save base
 REM vagrant snapshot restore base
 REM vagrant snapshot delete base
 REM vagrant snapshot list
-REM vagrant reload
 REM vagrant status
