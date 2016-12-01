e5506be97af828c0e72d27c5fbee42a4ed577a61
lisp719 <lisp719@hotmail.co.jp>
2016-12-02 01:00:31 +0900

    rm subl

--- linux_files/prov/gui.sh
+++ linux_files/prov/gui.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 sudo apt-get install -y \
-  emacs-mozc \
   fcitx-mozc \
   ttf-mona
 
@@ -15,14 +14,3 @@ mkdir ~/.config/fcitx
 cp ~/linux_files/fcitx.profile ~/.config/fcitx/profile
 
 sudo sed -i.bak -e "/autologin=/a autologin=vagrant" /etc/lxdm/lxdm.conf
-
-wget https://download.sublimetext.com/sublime-text_build-3114_amd64.deb
-sudo dpkg -i sublime-text_build-3114_amd64.deb
-rm sublime-text_build-3114_amd64.deb
-
-wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages/
-
-mkdir -p ~/.config/sublime-text-3/Packages/User/
-cp ~/linux_files/subl/* ~/.config/sublime-text-3/Packages/User/
-
-git config  --global  core.editor "subl -w"
