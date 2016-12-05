886de3e5d9d17f067c533ec9b627bfe970ba2958
lisp719 <lisp719@hotmail.co.jp>
2016-12-06 00:16:53 +0900

    db

--- linux_files/prov/db.sh
+++ linux_files/prov/db.sh
@@ -1,14 +1,10 @@
 #! /bin/sh
 
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y \
-  libpq-dev \
-  libmysqlclient-dev \
-  libsqlite3-dev \
-  mysql-server-5.6 \
-  postgresql
+sudo dnf install -y \
+  mariadb-server \
+  redis
 
-sudo sed -i.bak -e "/^\[mysqld\]$/a table_definition_cache = 400" /etc/mysql/my.cnf
-sudo sed -i.bak -e "/^\[mysqld\]$/a default-time-zone = '+0:00'" /etc/mysql/my.cnf
-sudo service mysql restart
-
-sudo su -c "createuser -s vagrant" postgres
+sudo systemctl start mariadb
+sudo systemctl start redis
+sudo systemctl enable mariadb
+sudo systemctl enable redis
