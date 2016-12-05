a5c5c05725575128135f80e58f77b347fc606bd9
lisp719 <lisp719@hotmail.co.jp>
2016-12-06 00:04:47 +0900

    rbenv

--- linux_files/prov/rbenv.sh
+++ linux_files/prov/rbenv.sh
@@ -1,11 +1,15 @@
 #! /bin/sh
 
-sudo apt-get install -y \
-  build-essential \
-  libreadline-dev \
-  libssl-dev \
-  libxml2-dev \
-  zlib1g-dev
+sudo dnf install -y \
+  gcc\
+  bzip2\
+  openssl-devel\
+  libyaml-devel\
+  libffi-devel\
+  readline-devel\
+  zlib-devel\
+  gdbm-devel\
+  ncurses-devel
 
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
