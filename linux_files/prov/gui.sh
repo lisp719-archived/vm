eb5f79c070d4a02973b56dfa98ed1ea4aca93e75
lisp719 <lisp719@hotmail.co.jp>
2016-12-06 13:50:00 +0900

    gui

--- linux_files/prov/gui.sh
+++ linux_files/prov/gui.sh
@@ -1,16 +1,25 @@
 #! /bin/sh
 
-sudo apt-get install -y \
-  fcitx-mozc \
-  ttf-mona
+sudo dnf install -y \
+  lxterminal \
+  openbox \
+  xorg-x11-xinit \
+  xorg-x11-drv-evdev \
+  xorg-x11-server-Xorg
 
-sudo apt-get install -y \
-  lxde
+# cp /etc/X11/xinit/xinitrc ~/.xinitrc
 
-mkdir -p ~/.config/lxsession/LXDE
-cp ~/linux_files/autostart ~/.config/lxsession/LXDE/
+# sudo apt-get install -y \
+#   fcitx-mozc \
+#   ttf-mona
 
-mkdir ~/.config/fcitx
-cp ~/linux_files/fcitx.profile ~/.config/fcitx/profile
+# sudo apt-get install -y \
+#   lxde
 
-sudo sed -i.bak -e "/autologin=/a autologin=vagrant" /etc/lxdm/lxdm.conf
+# mkdir -p ~/.config/lxsession/LXDE
+# cp ~/linux_files/autostart ~/.config/lxsession/LXDE/
+
+# mkdir ~/.config/fcitx
+# cp ~/linux_files/fcitx.profile ~/.config/fcitx/profile
+
+# sudo sed -i.bak -e "/autologin=/a autologin=vagrant" /etc/lxdm/lxdm.conf
