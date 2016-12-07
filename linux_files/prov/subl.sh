501a982a5379137e0d39ad26786aae863124bc72
lisp719 <test@example.com>
2016-12-07 13:16:29 +0900

    fix

--- linux_files/prov/subl.sh
+++ linux_files/prov/subl.sh
@@ -3,9 +3,10 @@
 sudo dnf install -y \
   pygobject3
 
-curl https://download.sublimetext.com/sublime_text_3_build_3126_x64.tar.bz2 -o /tmp/sublime.tar.bz2
-tar jxvf /tmp/sublime.tar.bz2
-sudo mv /tmp/sublime_text_3 /opt/
+cd /tmp
+curl https://download.sublimetext.com/sublime_text_3_build_3126_x64.tar.bz2 -o sublime.tar.bz2
+tar jxvf sublime.tar.bz2
+sudo mv sublime_text_3 /opt/
 sudo ln -s /opt/sublime_text_3/sublime_text /usr/bin/subl
 wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages/
 
