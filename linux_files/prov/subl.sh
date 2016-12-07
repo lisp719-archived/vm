699e376259eb515320217490453c835920cf592d
lisp719 <test@example.com>
2016-12-07 19:11:57 +0900

    fix subl

--- linux_files/prov/subl.sh
+++ linux_files/prov/subl.sh
@@ -10,7 +10,6 @@ sudo mv sublime_text_3 /opt/
 sudo ln -s /opt/sublime_text_3/sublime_text /usr/bin/subl
 wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages/
 
-cp -r ~/linux_files/conf/subl/* ~/.config/sublime-text-3/Packages/User/
-mv ~/.config/sublime-text-3/Packages/User/input ~/.config/sublime-text-3/Packages/
+cp -r ~/linux_files/conf/subl ~/.config/sublime-text-3/Packages
 
 git config --global core.editor "subl -w"
