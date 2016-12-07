eb9764354b0a2bcf3663cf5f86d367e40e85dde2
lisp719 <lisp719@hotmail.co.jp>
2016-12-08 01:18:40 +0900

    openbox

--- linux_files/prov/gui.sh
+++ linux_files/prov/gui.sh
@@ -7,15 +7,11 @@ sudo dnf install -y \
   google-droid-sans-mono-fonts \
   ibus-kkc \
   ipa-gothic-fonts \
-  i3 \
+  openbox \
+  tint2 \
   xorg-x11-xinit \
   xorg-x11-drv-evdev \
   xorg-x11-server-utils \
   xorg-x11-server-Xorg
 
 cp ~/linux_files/conf/.xinitrc ~
-
-echo keycode 102=Super_L > ~/.Xmodmap
-# xmodmap -pke > ~/.Xmodmap_default
-# xmodmap ~/.Xmodmap
-# xset -r 49
