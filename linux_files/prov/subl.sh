e5cb9ba2b760600dc44a432bca2afdc3123b610d
lisp719 <test@example.com>
2016-12-07 12:34:52 +0900

    subl

--- linux_files/prov/subl.sh
+++ linux_files/prov/subl.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+sudo dnf install -y \
+  pygobject3
+
 curl https://download.sublimetext.com/sublime_text_3_build_3126_x64.tar.bz2 -o /tmp/sublime.tar.bz2
 tar jxvf /tmp/sublime.tar.bz2
 sudo mv /tmp/sublime_text_3 /opt/
