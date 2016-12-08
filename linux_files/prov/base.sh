be09b5accfa55752a3296d4e6b296880fc4c5f31
lisp719 <lisp719@hotmail.co.jp>
2016-12-08 16:38:46 +0900

    base

--- linux_files/prov/base.sh
+++ linux_files/prov/base.sh
@@ -7,28 +7,22 @@ sudo dnf install -y \
   tmux \
   tree
 
+sudo timedatectl set-timezone Asia/Tokyo
+
+mkdir ~/bin ~/sandbox ~/projects
+
+git clone https://lisp719@bitbucket.org/lisp719/cli.git ~/bin/cli
+
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install --all
 
-sudo timedatectl set-timezone Asia/Tokyo
-
-git config --global user.name lisp719
-git config --global user.email test@example.com
-git config --global push.default simple
-git config --global core.editor nano
-git config --global core.excludesfile ~/.gitignore
 touch ~/.gitignore
-
-mkdir ~/bin
-git clone https://lisp719@bitbucket.org/lisp719/cli.git ~/bin/cli
-
-cp ~/linux_files/conf/.tmux.conf ~/.tmux.conf
 cp ~/linux_files/conf/.bashconf ~/.bashconf
+cp ~/linux_files/conf/.gitconfig ~/.gitconfig
+cp ~/linux_files/conf/.tmux.conf ~/.tmux.conf
 
 echo >> ~/.bashrc
 echo '. ~/.bashconf' >> ~/.bashrc
-
-mkdir ~/sandbox ~/projects
 
 cd ~/.ssh/
 cp /vagrant/.ssh/* .
