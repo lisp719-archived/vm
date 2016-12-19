a6aa0f5476fb7c0b202f371255d6d11ff6ecfbf3
lisp719 <test@example.com>
2016-12-19 14:10:36 +0900

    docker

--- linux_files/prov/docker.sh
+++ linux_files/prov/docker.sh
@@ -3,3 +3,8 @@
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl enable docker.service
 sudo systemctl start docker
+
+sudo usermod -aG docker vagrant
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
