ffcce346ce0af58ded998edd73da55527b8a068d
lisp719 <lisp719@hotmail.co.jp>
2017-04-22 22:29:45 +0900

    change terminal

--- linux_files/prov/gui.sh
+++ linux_files/prov/gui.sh
@@ -1,18 +1,23 @@
 #! /bin/sh
 
+set -e
+
 sudo dnf install -y \
   gitk \
   git-gui \
   google-droid-sans-mono-fonts \
   ibus-kkc \
   ipa-gothic-fonts \
-  lxterminal \
   openbox \
   spacefm \
   tint2 \
+  terminator \
   xorg-x11-xinit \
   xorg-x11-drv-evdev \
   xorg-x11-server-Xorg \
   urw-fonts
 
 cp ~/linux_files/conf/.xinitrc ~
+
+mkdir -p ~/.config
+mv ~/linux_files/conf/terminator ~/.config
