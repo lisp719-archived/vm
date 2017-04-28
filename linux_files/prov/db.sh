c0d07f9ec4e4c132fdb8c05709b8a8585d93d4fc
lisp719 <test@example.com>
2017-04-28 17:04:05 +0900

    postgres

--- linux_files/prov/db.sh
+++ linux_files/prov/db.sh
@@ -1,13 +1,24 @@
 #! /bin/sh
 
+set -e
+
 sudo dnf install -y \
   mariadb-server \
   mariadb-devel \
+  postgresql-server \
   redis \
   sqlite \
   sqlite-devel
 
+sudo postgresql-setup --initdb
+
 sudo systemctl start mariadb
+sudo systemctl start postgresql
 sudo systemctl start redis
 sudo systemctl enable mariadb
+sudo systemctl enable postgresql
 sudo systemctl enable redis
+
+cd /tmp
+sudo su -c "createuser -s vagrant" postgres
+createdb vagrant
