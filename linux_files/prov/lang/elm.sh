b0b2d02a3ca193334426e3f3ae787a5bf9f2d8a3
lisp719 <lisp719@hotmail.co.jp>
2017-05-17 22:05:52 +0900

    elm format

--- linux_files/prov/lang/elm.sh
+++ linux_files/prov/lang/elm.sh
@@ -1,9 +1,8 @@
 #! /bin/sh
 
 sudo dnf install -y ncurses-compat-libs
-npm i -g elm elm-oracle
 
-cd /tmp
-wget https://github.com/avh4/elm-format/releases/download/0.5.2-alpha/elm-format-0.18-0.5.2-alpha-linux-x64.tgz
-tar zxvf elm-format-0.18-0.5.2-alpha-linux-x64.tgz
-mv elm-format ~/bin
+npm i -g \
+  elm \
+  elm-format \
+  elm-oracle
