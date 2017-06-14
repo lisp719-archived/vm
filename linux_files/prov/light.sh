a50b3f6409e7ccb9334835174933cbff400781b6
lisp719 <lisp719@hotmail.co.jp>
2017-06-14 23:23:56 +0900

    light table

---
+++ linux_files/prov/light.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+set -e
+
+sudo dnf install -y \
+  alsa-lib \
+  GConf2
+
+cd /tmp
+wget https://github.com/LightTable/LightTable/releases/download/0.8.1/lighttable-0.8.1-linux.tar.gz -O lighttable.tar.gz
+atool -x lighttable.tar.gz
+sudo mv lighttable-0.8.1-linux /opt/lighttable
+sudo ln -s /opt/lighttable/light /usr/bin/
