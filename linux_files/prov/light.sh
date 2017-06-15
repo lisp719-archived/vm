7e39e6f3c80cb42300c67f059f63c4eb214b50a6
lisp719 <test@example.com>
2017-06-15 15:27:56 +0900

    var

--- linux_files/prov/light.sh
+++ linux_files/prov/light.sh
@@ -6,8 +6,10 @@ sudo dnf install -y \
   alsa-lib \
   GConf2
 
+url=https://github.com/LightTable/LightTable/releases/download/0.8.1/lighttable-0.8.1-linux.tar.gz
+
 cd /tmp
-wget https://github.com/LightTable/LightTable/releases/download/0.8.1/lighttable-0.8.1-linux.tar.gz -O lighttable.tar.gz
+wget $url -O lighttable.tar.gz
 atool -x lighttable.tar.gz
 sudo mv lighttable-0.8.1-linux /opt/lighttable
 sudo ln -s /opt/lighttable/light /usr/bin/
