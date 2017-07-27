5c76075faaf81856a3b17b2cf04b2addc6ac22ed
lisp719 <lisp719@hotmail.co.jp>
2017-07-27 22:38:23 +0900

    update docker-compose

--- linux_files/prov/docker.sh
+++ linux_files/prov/docker.sh
@@ -6,5 +6,5 @@ sudo systemctl start docker
 
 sudo usermod -aG docker vagrant
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.14.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
