72208c63e3706122dc63a06cb01d431a2af29211
lisp719 <lisp719@hotmail.co.jp>
2017-08-31 23:54:42 +0900

    install subl from dnf

--- linux_files/prov/subl.sh
+++ linux_files/prov/subl.sh
@@ -1,15 +1,13 @@
 #! /bin/sh
 
+sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
+sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+
 sudo dnf install -y \
-  pygobject3
+  pygobject3 \
+  sublime-text
 
-cd /tmp
-curl https://download.sublimetext.com/sublime_text_3_build_3126_x64.tar.bz2 -o sublime.tar.bz2
-tar jxvf sublime.tar.bz2
-sudo mv sublime_text_3 /opt/
-sudo ln -s /opt/sublime_text_3/sublime_text /usr/bin/subl
 wget https://packagecontrol.io/Package%20Control.sublime-package -P ~/.config/sublime-text-3/Installed\ Packages/
-
 cp -r ~/linux_files/conf/subl ~/.config/sublime-text-3/Packages
 
 git config --global core.editor "subl -w"
