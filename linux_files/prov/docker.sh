2d3a7a9d643d359a11f53b75456b3fdb8fc23092
lisp719 <lisp719@hotmail.co.jp>
2017-09-18 03:26:28 +0900

    rm start docker

--- linux_files/prov/docker.sh
+++ linux_files/prov/docker.sh
@@ -2,7 +2,6 @@
 
 curl -fsSL https://get.docker.com/ | sh
 sudo systemctl enable docker.service
-sudo systemctl start docker
 
 sudo usermod -aG docker vagrant
 sudo curl -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
