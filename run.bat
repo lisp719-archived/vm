a97f12c565704836807688cc1f2962a6e6a6a937
lisp719 <lisp719@hotmail.co.jp>
2017-09-18 02:27:07 +0900

    slim task

--- run.bat
+++ run.bat
@@ -1,14 +1,4 @@
-time /t
-
-REM vagrant destroy -f
-REM vagrant up
-REM vagrant reload
-REM timeout 10
-REM vagrant provision
-REM vagrant snapshot save base
-REM vagrant snapshot restore base
-REM vagrant snapshot delete base
-REM vagrant snapshot list
-REM vagrant status
-
-time /t
+vagrant halt
+vagrant destroy -f
+vagrant up
+vagrant snapshot save init
