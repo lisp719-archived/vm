3eeec84f8006481425fceaa8b5eeab66d899dab9
lisp719 <lisp719@hotmail.co.jp>
2017-11-05 15:23:12 +0900

    vscode

--- linux_files/prov/editor/vscode.sh
+++ linux_files/prov/editor/vscode.sh
@@ -7,8 +7,11 @@ dnf check-update
 sudo dnf install -y code
 
 mkdir -p ~/.config/Code/User
+wget https://bitbucket.org/lisp719/win/raw/master/vscode/keybindings.json -O ~/.config/Code/User/keybindings.json
+wget https://bitbucket.org/lisp719/win/raw/master/vscode/locale.json -O ~/.config/Code/User/locale.json
 wget https://bitbucket.org/lisp719/win/raw/master/vscode/settings.json -O ~/.config/Code/User/settings.json
 
 code --install-extension Ionide.Ionide-fsharp
+code --install-extension lucax88x.codeacejumper
 code --install-extension ms-vscode.sublime-keybindings
 code --install-extension sbrink.elm
