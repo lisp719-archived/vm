36c6f92b5694c78f2f39b8578e6c9b26afa5d4e9
lisp719 <lisp719@hotmail.co.jp>
2017-11-24 22:01:39 +0900

    node

--- linux_files/prov/lang/node.sh
+++ linux_files/prov/lang/node.sh
@@ -1,5 +1,12 @@
 #! /bin/sh
 
+set -e
+
+export N_PREFIX="$HOME/.n"
+
 curl -L https://git.io/n-install | bash -s -- -y
+
+. ~/.bashrc
+npm i -g http-server prettier
 
 cp ~/linux_files/conf/.prettierrc ~/.prettierrc
