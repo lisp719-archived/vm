4fd901c861db15e18200875b8757eed44c15b81b
lisp719 <lisp719@hotmail.co.jp>
2017-12-18 12:41:54 +0900

    psql

--- linux_files/prov/db.sh
+++ linux_files/prov/db.sh
@@ -5,11 +5,20 @@ set -e
 sudo dnf install -y \
   mariadb-server \
   mariadb-devel \
+  postgresql-devel \
+  postgresql-server \
   redis \
   sqlite \
   sqlite-devel
 
 sudo systemctl start mariadb
 sudo systemctl enable mariadb
+
 sudo systemctl start redis
 sudo systemctl enable redis
+
+sudo postgresql-setup --initdb
+sudo systemctl start postgresql
+sudo systemctl enable postgresql
+sudo su - -c "createuser -s vagrant" postgres
+createdb vagrant
