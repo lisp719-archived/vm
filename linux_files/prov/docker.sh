afb15df2a7a87b8e85a75a735670bd28483e703e
lisp719 <test@example.com>
2018-01-24 23:15:09 +0900

    update docker-compose

--- linux_files/prov/docker.sh
+++ linux_files/prov/docker.sh
@@ -9,7 +9,7 @@ sudo dnf install -y docker-ce
 sudo systemctl enable docker.service
 sudo usermod -aG docker vagrant
 
-sudo curl -L "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 git clone git@bitbucket.org:lisp719/docker.git ~/docker
