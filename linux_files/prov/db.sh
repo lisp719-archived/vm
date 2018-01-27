9d3e564da5b45c8defe9bfe8ef332dab940b3da5
lisp719 <test@example.com>
2018-01-27 16:49:25 +0900

    postgresを削除しました

--- linux_files/prov/db.sh
+++ linux_files/prov/db.sh
@@ -5,8 +5,6 @@ set -e
 sudo dnf install -y \
   mariadb-server \
   mariadb-devel \
-  postgresql-devel \
-  postgresql-server \
   redis \
   sqlite \
   sqlite-devel
@@ -16,9 +14,3 @@ sudo systemctl enable mariadb
 
 sudo systemctl start redis
 sudo systemctl enable redis
-
-sudo postgresql-setup --initdb
-sudo systemctl start postgresql
-sudo systemctl enable postgresql
-sudo su - -c "createuser -s vagrant" postgres
-createdb vagrant
