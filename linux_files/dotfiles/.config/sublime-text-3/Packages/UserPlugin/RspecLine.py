8b289255f5b96a53af31f942ea5054b3b1fb3488
lisp719 <test@example.com>
2018-03-03 16:25:55 +0900

    [modify][sublime] docker経由でrspecを走らせるようにしました

--- linux_files/dotfiles/.config/sublime-text-3/Packages/UserPlugin/RspecLine.py
+++ linux_files/dotfiles/.config/sublime-text-3/Packages/UserPlugin/RspecLine.py
@@ -6,7 +6,8 @@ class RspecLineCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
         project_hash = self.view.window().extract_variables()
-        shell_cmd = 'bundle exec rspec {0}:{1}'.format(project_hash['file'], line_number)
+        relative_file = project_hash['file'].replace(project_hash['folder'] + '/', '')
+        shell_cmd = 'docker-compose run app rspec {0}:{1}'.format(relative_file, line_number)
 
         command_hash = {
             'shell_cmd': shell_cmd,
