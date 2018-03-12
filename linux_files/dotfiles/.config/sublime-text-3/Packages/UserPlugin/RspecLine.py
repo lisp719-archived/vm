957317cd848ed624004b97e04d87d4d99a537117
lisp719 <test@example.com>
2018-03-12 16:54:04 +0900

    [modify] sublimeからrspecを流す時、絶対パスを使うようにしました

--- linux_files/dotfiles/.config/sublime-text-3/Packages/UserPlugin/RspecLine.py
+++ linux_files/dotfiles/.config/sublime-text-3/Packages/UserPlugin/RspecLine.py
@@ -6,8 +6,7 @@ class RspecLineCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         line_number = self.view.rowcol(self.view.sel()[0].begin())[0] + 1
         project_hash = self.view.window().extract_variables()
-        relative_file = project_hash['file'].replace(project_hash['folder'] + '/', '')
-        shell_cmd = 'docker-compose run app rspec {0}:{1}'.format(relative_file, line_number)
+        shell_cmd = 'docker-compose run app rspec {0}:{1}'.format(project_hash['file'], line_number)
 
         command_hash = {
             'shell_cmd': shell_cmd,
