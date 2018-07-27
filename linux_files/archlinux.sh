e14f21b6348dfffaf7e3243fcb270be303321d56
lisp719 <lisp719@gmail.com>
2018-07-27 21:28:46 +0900

    [modify] degitをインストールするようにしました

--- linux_files/archlinux.sh
+++ linux_files/archlinux.sh
@@ -56,7 +56,7 @@ curl 'http://vim-bootstrap.com/generate.vim' -d 'editor=vim' > ~/.vimrc
 curl -sL https://raw.github.com/simonwhitaker/gibo/master/gibo -o ~/bin/gibo && chmod +x ~/bin/gibo && ~/bin/gibo update
 test -d ~/.emacs.d || git clone --depth 1 https://github.com/syl20bnr/spacemacs ~/.emacs.d
 sudo cp -r  ~/linux_files/system/* /etc/systemd/system/
-sudo npm i -g http-server npm prettier
+sudo npm i -g degit http-server npm prettier
 sudo systemctl enable docker
 sudo systemctl enable sddm
 sudo usermod -aG docker vagrant
