4f962c1c3a5c411c5128f26e92b8f6f0e770bd2f
lisp719 <lisp719@gmail.com>
2019-12-28 01:18:08 +0900

    [vscode] calvaを削除

--- linux_files/archlinux.sh
+++ linux_files/archlinux.sh
@@ -58,7 +58,6 @@ yay -S --needed --noconfirm \
 # after package
 . /usr/share/nvm/init-nvm.sh && nvm install --lts && npm i -g http-server npm-check prettier
 code --install-extension Angular.ng-template
-code --install-extension betterthantomorrow.calva
 code --install-extension blanu.vscode-styled-jsx
 code --install-extension esbenp.prettier-vscode
 code --install-extension ms-vscode-remote.remote-containers
