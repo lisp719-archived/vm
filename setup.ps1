386b09037c55fa4b5b898e15bbcd30d1dcf08ae3
lisp719 <lisp719@gmail.com>
2020-09-19 17:28:59 +0900

    feat: Create setup script

---
+++ setup.ps1
@@ -0,0 +1,42 @@
+$box_url = "https://app.vagrantup.com/archlinux/boxes/archlinux/versions/2020.08.12/providers/virtualbox.box"
+$box_path = "tmp/box.tar"
+$ovf_path = "tmp/box.ovf"
+$vm_name = "foo"
+
+
+Function FetchOvf() {
+  mkdir tmp
+  Invoke-WebRequest $box_url -o $box_path
+  tar -xf $box_path -C tmp
+}
+
+Function CreateVm() {
+  $ovf_file = [xml](Get-Content $ovf_path)
+  $vmdk_name = [System.IO.Path]::GetFileNameWithoutExtension($ovf_file.Envelope.References.File.href)
+  $vdi_path = "{0}/VirtualBox VMs/{1}/{2}.vdi" -f $HOME, $vm_name, $vmdk_name
+  $original_vm_name = $ovf_file.Envelope.VirtualSystem.Machine.name
+
+  VBoxManage import $ovf_path --options importtovdi
+  vboxmanage modifyvm $original_vm_name --name $vm_name
+
+  VBoxManage modifymedium $vdi_path --resize 81920
+}
+
+Function ModifyVm() {
+  VBoxManage modifyvm $vm_name --memory 2048
+  VBoxManage modifyvm $vm_name --cpus 2
+  VBoxManage modifyvm $vm_name --defaultfrontend headless
+
+  VBoxManage modifyvm $vm_name --natpf1 "ssh,tcp,,2222,,22"
+  VBoxManage modifyvm $vm_name --natpf1 "tcp3000,tcp,,3000,,3000"
+  VBoxManage modifyvm $vm_name --natpf1 "tcp3035,tcp,,3035,,3035"
+  VBoxManage modifyvm $vm_name --natpf1 "tcp8000,tcp,,8000,,8000"
+  VBoxManage modifyvm $vm_name --natpf1 "tcp8080,tcp,,8080,,8080"
+
+  VBoxManage sharedfolder add $vm_name --name linux_files --hostpath ./linux_files --automount
+  VBoxManage sharedfolder add $vm_name --name vagrant --hostpath /sync --automount
+}
+
+# FetchOvf
+CreateVm
+ModifyVm
