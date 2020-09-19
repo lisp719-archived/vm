3b49a037f649b05f2920cc8ab05ba702e16c6231
lisp719 <lisp719@gmail.com>
2020-09-19 21:29:58 +0900

    feat: Fix mount point

--- linux_files/archlinux.sh
+++ linux_files/archlinux.sh
@@ -2,16 +2,21 @@
 
 set -e
 
+# mount
+mkdir -p ~/tmp_linux_files ~/tmp_sync
+sudo mount -t vboxsf linux_files ~/tmp_linux_files
+sudo mount -t vboxsf sync ~/tmp_sync
+
 # common
-~/linux_files/cp_dotfiles.sh
-cp /vagrant/.ssh/* ~/.ssh/ && chmod 600 ~/.ssh/*
+cp -r ~/tmp_linux_files/dotfiles/.[!.]* ~
+cp ~/tmp_sync/.ssh/* ~/.ssh/ && chmod 600 ~/.ssh/*
 mkdir -p ~/bin ~/code
 sudo timedatectl set-timezone Asia/Tokyo
 sudo rm /etc/resolv.conf && echo nameserver 8.8.8.8 | sudo tee /etc/resolv.conf
 
 # package
-sudo cp ~/linux_files/mirrorlist /etc/pacman.d/mirrorlist
-sudo pacman -Syyu
+sudo cp ~/tmp_linux_files/mirrorlist /etc/pacman.d/mirrorlist
+sudo pacman -Syyu --noconfirm
 sudo pacman -S --noconfirm --needed \
   atool \
   base-devel \
@@ -39,9 +44,14 @@ yay -S --needed --noconfirm \
 
 # after package
 curl 'http://vim-bootstrap.com/generate.vim' -d 'editor=vim' > ~/.vimrc
-sudo cp -r  ~/linux_files/system/* /etc/systemd/system/
+sudo cp -r  ~/tmp_linux_files/system/* /etc/systemd/system/
 sudo systemctl enable docker
 sudo usermod -aG docker vagrant
 sudo usermod -aG vboxsf vagrant
+
+# umount
+sudo umount ~/tmp_linux_files/
+sudo umount ~/tmp_sync/
+rmdir ~/tmp_linux_files ~/tmp_sync
 
 echo success
